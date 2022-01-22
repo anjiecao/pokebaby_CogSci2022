f55966234e20979dfdcefce73314cd9a6713a7b4
anjie cao <anjiecao@stanford.edu>
2022-01-22 10:29:58 -0800

    reorg, new plots

--- model/helper/simulation_results_processing.R
+++ model/helper/simulation_results_processing.R
@@ -21,6 +21,29 @@ tidy_up_raw_simulation_results <- function(df, im_type){
       select(params_info, complexity, trial_number, sequence_scheme, sequence_scheme_print,surprisal, log_surprisal, kl, log_kl)
     
     
+  }else if (im_type == "random"){
+  
+    tidy_df <- df %>% 
+      mutate(
+        trial_number = stimulus_idx, 
+        log_sample_n = log(sample_n),
+        sequence_scheme =  sub(".*_", "", stim_info),
+        stimuli_rep = str_replace(stim_info, paste0("_", "ss_", sequence_scheme), ""), 
+        complexity = case_when(
+          stimuli_rep == "nf_6_of_1" ~ "simple", 
+          stimuli_rep == "nf_6_of_3" ~ "complex"
+        ), 
+        sequence_scheme_print = case_when(
+          sequence_scheme == "BBBBBB" ~ "No Deviant", 
+          sequence_scheme == "BDBBBB" ~ "Deviant at 2nd Trial", 
+          sequence_scheme == "BBBDBB" ~ "Deviant at 4th Trial", 
+          sequence_scheme == "BBBBBD" ~ "Deviant at Last Trial")
+      ) %>% 
+      ungroup() %>% 
+      mutate(params_info = NA) %>% 
+      select(params_info, complexity, trial_number, sequence_scheme, sequence_scheme_print, sample_n, log_sample_n) %>% 
+      mutate(im_type = im_type)
+  
   }else{
     tidy_df <- df %>% 
       mutate(
@@ -79,24 +102,52 @@ summarise_scaled_tidy_sim_res <- function(scaled_tidy_df){
 # ------------------ scaling the model results to match mean and sd of the behavioral results according to the best fit parameters   ------------------  #
 
 scale_model_results <- function(behavioral_df, sim_df, best_params){
-  scaled_sim_df <- behavioral_df %>% 
-    left_join(sim_df %>% filter(params_info == best_params), by = c("trial_number", "complexity", "sequence_scheme", "sequence_scheme_print")) %>% 
-    group_by(params_info) %>% 
-    summarise(
-      mean_log_trial_sim_res = mean(log_sample_n),  # m1
-      sd_log_trial_sim_res = sd(log_sample_n), # s1
-      mean_log_trial_looking_time = mean(log(trial_looking_time)), # m2
-      sd_log_trial_looking_time = sd(log(trial_looking_time)) # s2
-    ) %>% 
-    left_join(sim_df, by = "params_info") %>% 
-    mutate(
-      multiply_const = sd_log_trial_looking_time / sd_log_trial_sim_res # s2/s1
-    ) %>% 
-    mutate(
-      scaled_log_sample_n = mean_log_trial_looking_time + (log_sample_n - mean_log_trial_sim_res) * multiply_const
-    ) %>% 
-    filter(!is.na(complexity)) %>% 
-    select(im_type, params_info, complexity, trial_number, sequence_scheme, sequence_scheme_print, sample_n, log_sample_n, scaled_log_sample_n)
+  
+  if(is.na(best_params)){
+    scaled_sim_df <- behavioral_df %>% 
+      left_join(sim_df, by = c("trial_number", "complexity", "sequence_scheme", "sequence_scheme_print")) %>% 
+      group_by(params_info) %>% 
+      summarise(
+        mean_log_trial_sim_res = mean(log_sample_n),  # m1
+        sd_log_trial_sim_res = sd(log_sample_n), # s1
+        mean_log_trial_looking_time = mean(log(trial_looking_time)), # m2
+        sd_log_trial_looking_time = sd(log(trial_looking_time)) # s2
+      ) %>% 
+      left_join(sim_df, by = "params_info") %>% 
+      mutate(
+        multiply_const = sd_log_trial_looking_time / sd_log_trial_sim_res # s2/s1
+      ) %>% 
+      mutate(
+        scaled_log_sample_n = mean_log_trial_looking_time + (log_sample_n - mean_log_trial_sim_res) * multiply_const
+      ) %>% 
+      filter(!is.na(complexity)) %>% 
+      select(im_type, params_info, complexity, trial_number, sequence_scheme, sequence_scheme_print, sample_n, log_sample_n, scaled_log_sample_n)
+    
+    
+  }else{
+    
+    scaled_sim_df <- behavioral_df %>% 
+      left_join(sim_df %>% filter(params_info == best_params), by = c("trial_number", "complexity", "sequence_scheme", "sequence_scheme_print")) %>% 
+      group_by(params_info) %>% 
+      summarise(
+        mean_log_trial_sim_res = mean(log_sample_n),  # m1
+        sd_log_trial_sim_res = sd(log_sample_n), # s1
+        mean_log_trial_looking_time = mean(log(trial_looking_time)), # m2
+        sd_log_trial_looking_time = sd(log(trial_looking_time)) # s2
+      ) %>% 
+      left_join(sim_df, by = "params_info") %>% 
+      mutate(
+        multiply_const = sd_log_trial_looking_time / sd_log_trial_sim_res # s2/s1
+      ) %>% 
+      mutate(
+        scaled_log_sample_n = mean_log_trial_looking_time + (log_sample_n - mean_log_trial_sim_res) * multiply_const
+      ) %>% 
+      filter(!is.na(complexity)) %>% 
+      select(im_type, params_info, complexity, trial_number, sequence_scheme, sequence_scheme_print, sample_n, log_sample_n, scaled_log_sample_n)
+    
+    
+  }
+  
   
   return(scaled_sim_df)
 }
